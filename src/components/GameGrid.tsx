545f5aea9c4ddd5022e667cc29e5308e53e96f3e
Quan Pham <Quan.Pham@vklink.vn>
2023-09-19 17:49:31 +0700

    fetching genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -20,12 +20,12 @@ const GameGrid = () => {
       >
         {isLoading &&
           skeletons.map((skeleton) => (
-            <GameCardContainer>
+            <GameCardContainer key={skeleton}>
               <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
           ))}
         {games.map((game) => (
-          <GameCardContainer>
+          <GameCardContainer key={game.id}>
             <GameCard game={game}></GameCard>
           </GameCardContainer>
         ))}
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -32,7 +32,11 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color="gray.500"></Icon>
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color="gray.500"
+        ></Icon>
       ))}
     </HStack>
   );
