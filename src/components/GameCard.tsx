071e674ddcde885b0cbadf515b31a326566b8a37
Quan Pham <Quan.Pham@vklink.vn>
2023-09-19 16:27:34 +0700

    optimized image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,8 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import gerCroppedImageUrl from "../services/mage-url";
+
 interface Props {
   game: Game;
 }
@@ -10,14 +12,14 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image}></Image>
+      <Image src={gerCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           ></PlatformIconList>
-          <CriticScore score={game.metacritic}></CriticScore>
+          <CriticScore score={game.metaCritic}></CriticScore>
         </HStack>
       </CardBody>
     </Card>
