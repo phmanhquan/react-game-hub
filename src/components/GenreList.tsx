0f0b208be4e623869aa43d02e67f89321732a21d
Quan Pham <Quan.Pham@vklink.vn>
2023-09-20 09:52:11 +0700

    fetching data for genres and game

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import useGenres from "../hooks/useGenres";
+import useGenres, { Genres } from "../hooks/useGenres";
+import useData from "../hooks/useData";
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { data } = useData<Genres>("/genres");
   return (
     <ul>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <li key={genre.id}>{genre.name}</li>
       ))}
     </ul>
